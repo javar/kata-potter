9fd40c1e64123c7e67e3e0daef574a009a864b9b
Javier Aristizabal <javieraristizabal@gmail.com>
2015-02-12 20:09:55 +0100

    Extract new method

--- price_calculator.rb
+++ price_calculator.rb
@@ -11,12 +11,16 @@ class PriceCalculator
     	books.count * BOOK_PRICE
 	end
 
+  def sum_splited(group, size)
+    basket = Basket.new
+    basket.split(group, size).map{|books_splited|
+      partial_total(books_splited) 
+    }.reduce(:+)
+  end
+
 	def min_price(group)
-  		basket = Basket.new
   		(0..group.length).map{|size|
-    		basket.split(group, size).map{|books_splited|
-      		partial_total(books_splited) 
-    		}.reduce(:+)
+    		sum_splited(group, size)
   		}.min    
 	end
 end
