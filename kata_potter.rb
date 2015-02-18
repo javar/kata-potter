a696d9ede6fd773bf2809845d646a037ab796143
Javier Aristizabal <javieraristizabal@gmail.com>
2015-02-18 11:57:12 +0100

    Better method name

--- kata_potter.rb
+++ kata_potter.rb
@@ -3,10 +3,10 @@ require_relative "./basket.rb"
 
 def basket(books)
   basket = Basket.new
-  basket.generate_all_available(books).map{|basket| basket_price(basket)}.min
+  basket.generate_all_available(books).map{|basket| price(basket)}.min
 end
 
-def basket_price (basket)
+def price (basket)
   price_calculator = PriceCalculator.new
   price_calculator.min_price(basket)
 end
